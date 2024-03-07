82c4fdb6a256a614b731f26facc2d4ecdf7060f5
Naihe0 <yangningrui@gmail.com>
2024-03-07 13:10:04 -0800

    qa back and front

--- frontend/src/questionaire.tsx
+++ frontend/src/questionaire.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Typography, Input, Button, Row, Col, Form, Card } from 'antd';
+import { Typography, Input, Button, Row, Col, Form, Card, Select } from 'antd';
 import './questionaire.css';
 
 const { Title } = Typography;
 const { TextArea } = Input;
+const { Option } = Select;
 
 interface FormValues {
     title: string;
@@ -17,8 +18,8 @@ interface FormValues {
     question1?: string;
     question2?: string;
     question3?: string;
-  }
-  
+}
+
 
 export const App: React.FC = () => {
     const [form] = Form.useForm();
@@ -27,7 +28,7 @@ export const App: React.FC = () => {
     //     return form.isFieldsTouched(true) && form.getFieldsError().filter(({ errors }) => errors.length).length === 0;
     // };
 
-    const onFinish = async (values : FormValues) => {
+    const onFinish = async (values: FormValues) => {
         console.log('Received values of form: ', values);
         try {
             const response = await fetch('/questionaire', {
@@ -63,113 +64,174 @@ export const App: React.FC = () => {
                     onFinish={onFinish}
                     onFinishFailed={errorInfo => console.log('Failed:', errorInfo)}
                 >
-                    <Row gutter={16}>
-                        <Col span={8}>
-                            <Form.Item
-                                label="Title"
-                                name="title"
-                                rules={[{ required: true, message: 'Please input your title!' }]}
-                            >
-                                <Input placeholder="Title" />
-                            </Form.Item>
-                        </Col>
-                        <Col span={8}>
-                            <Form.Item
-                                label="First Name"
-                                name="firstName"
-                                rules={[{ required: true, message: 'Please input your first name!' }]}
-                            >
-                                <Input placeholder="First name" />
-                            </Form.Item>
-                        </Col>
-                        <Col span={8}>
-                            <Form.Item
-                                label="Last Name"
-                                name="lastName"
-                                rules={[{ required: true, message: 'Please input your last name!' }]}
-                            >
-                                <Input placeholder="Last name" />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <Row gutter={16}>
-                        <Col span={8}>
-                            <Form.Item
-                                label="Institution Name"
-                                name="institutionName"
-                                rules={[{ required: true, message: 'Please input your institution name!' }]}
-                            >
-                                <Input placeholder="Institution Name" />
-                            </Form.Item>
-                        </Col>
-                        <Col span={8}>
-                            <Form.Item
-                                label="State"
-                                name="state"
-                                rules={[{ required: true, message: 'Please input your state!' }]}
-                            >
-                                <Input placeholder="State" />
-                            </Form.Item>
-                        </Col>
-                        <Col span={8}>
-                            <Form.Item
-                                label="City"
-                                name="city"
-                                rules={[{ required: true, message: 'Please input your city!' }]}
-                            >
-                                <Input placeholder="City" />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <Row gutter={16}>
-                        <Col span={8}>
-                            <Form.Item
-                                label="Phone Number"
-                                name="phoneNumber"
-                                rules={[{ required: true, message: 'Please input your phone number!' }]}
-                            >
-                                <Input placeholder="Phone number" />
-                            </Form.Item>
-                        </Col>
-                        <Col span={8}>
-                            <Form.Item
-                                label="Email Address"
-                                name="email"
-                                rules={[{ required: true, message: 'Please input your email address!' }, { type: 'email', message: 'The input is not valid E-mail!' }]}
-                            >
-                                <Input placeholder="Email Address" />
-                            </Form.Item>
-                        </Col>
-                        {/* <Col span={8}>
-                            <Form.Item shouldUpdate={true}>
-                                {() => (
-                                    <Button
-                                        type="primary"
-                                        htmlType="submit"
-                                        // Only enable the Match button if the form is filled out
-                                        // disabled={!isFormFilled()}
-                                    >
-                                        Match
-                                    </Button>
-                                )}
-                            </Form.Item>
-                        </Col> */}
-                    </Row>
-                <div className="questions">
-                    <Title level={3}>Questions</Title>
-                    <Form.Item label="Question 1" name="question1">
-                        <TextArea rows={4} placeholder="Enter here" />
+                    <Form.Item
+                        label="Title"
+                        name="title"
+                        rules={[{ required: true, message: 'Please select your title!' }]}
+                    >
+                        <Select placeholder="Select a title">
+                            <Option value="mr">Mr</Option>
+                            <Option value="mrs">Mrs</Option>
+                            <Option value="ms">Ms</Option>
+                            <Option value="dr">Dr</Option>
+                            {/* Add more options as needed */}
+                        </Select>
                     </Form.Item>
-                    <Form.Item label="Question 2" name="question2">
-                        <TextArea rows={4} placeholder="Enter here" />
+
+                    <Form.Item
+                        label="First Name"
+                        name="firstName"
+                        rules={[{ required: true, message: 'Please input your first name!' }]}
+                    >
+                        <Input />
                     </Form.Item>
-                    <Form.Item label="Question 3" name="question3">
-                        <TextArea rows={4} placeholder="Enter here" />
+
+                    <Form.Item
+                        label="Last Name"
+                        name="lastName"
+                        rules={[{ required: true, message: 'Please input your last name!' }]}
+                    >
+                        <Input />
                     </Form.Item>
-                </div>
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
+                    <Form.Item
+                        label="Pronouns"
+                        name="pronouns"
+                        rules={[{ required: true, message: 'Please select your pronouns!' }]}
+                    >
+                        <Select placeholder="Select pronouns">
+                            <Option value="he/him">He/Him</Option>
+                            <Option value="she/her">She/Her</Option>
+                            <Option value="they/them">They/Them</Option>
+                            <Option value="other">Other</Option>
+                        </Select>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Institute"
+                        name="institute"
+                        rules={[{ required: true, message: 'Please input your institute!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="State"
+                        name="state"
+                        rules={[{ required: true, message: 'Please select your state!' }]}
+                    >
+                        <Select placeholder="Select a state">
+                            {/* Replace the options below with actual states */}
+                            <Option value="state1">State 1</Option>
+                            <Option value="state2">State 2</Option>
+                        </Select>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="City"
+                        name="city"
+                        rules={[{ required: true, message: 'Please input your city!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Zip"
+                        name="zip"
+                        rules={[{ required: true, message: 'Please input your zip code!' }]}
+                    >
+                        <Input placeholder="Zip code" />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Phone Number"
+                        name="phoneNumber"
+                        rules={[{ required: true, message: 'Please input your phone number!' }]}
+                    >
+                        <Input placeholder="Phone number" />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[
+                            { required: true, message: 'Please input your email!' },
+                            { type: 'email', message: 'Please enter a valid email!' },
+                        ]}
+                    >
+                        <Input placeholder="Email" />
+                    </Form.Item>
+
+                    <div className="Engagement">
+                        <Title level={3}>Engagement</Title>
+                        <Form.Item
+                            label="Engagement (A)"
+                            name="engagementA"
+                            rules={[{ required: true, message: 'Please select your engagement type!' }]}
+                        >
+                            <Select placeholder="Select an engagement type">
+                                <Option value="inPersonMeeting">In-Person Meeting</Option>
+                                <Option value="virtualMeeting">Virtual Meeting</Option>
+                                <Option value="phoneCall">Phone Call</Option>
+                                <Option value="conference">Conference</Option>
+                                <Option value="advisoryBoard">Advisory Board</Option>
+                            </Select>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Function"
+                            name="functionA"
+                            rules={[{ required: true, message: 'Please select a function!' }]}
+                        >
+                            <Select placeholder="Select a function">
+                                <Option value="r&d">R&D</Option>
+                                <Option value="devOps">Development Operations</Option>
+                                <Option value="medical">Medical</Option>
+                                <Option value="commercial">Commercial</Option>
+                            </Select>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Notes"
+                            name="notes"
+                        >
+                            <Input.TextArea rows={4} placeholder="Enter notes here" />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Follow-up Requested (B)"
+                            name="followUpRequested"
+                            rules={[{ required: true, message: 'Please select your follow-up engagement type!' }]}
+                        >
+                            <Select placeholder="Select a follow-up engagement type">
+                                <Option value="inPersonMeeting">In-Person Meeting</Option>
+                                <Option value="virtualMeeting">Virtual Meeting</Option>
+                                <Option value="phoneCall">Phone Call</Option>
+                            </Select>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Function"
+                            name="functionB"
+                            rules={[{ required: true, message: 'Please select a function!' }]}
+                        >
+                            <Select placeholder="Select a function">
+                                <Option value="r&d">R&D</Option>
+                                <Option value="devOps">Development Operations</Option>
+                                <Option value="medical">Medical</Option>
+                                <Option value="commercial">Commercial</Option>
+                            </Select>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Information Requested"
+                            name="informationRequested"
+                        >
+                            <Input.TextArea rows={4} placeholder="Enter information requested here" />
+                        </Form.Item>
+                    </div>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
                 </Form>
             </Card>
         </main>
