10ac13ef3ecabfe826a188c10e8672533f4220ff
Naihe0 <yangningrui@gmail.com>
2024-03-07 17:40:14 -0800

    qa back

--- backend/questionnaire.py
+++ backend/questionnaire.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, send_from_directory, request, jsonify
 from flask_cors import CORS
 import mysql.connector
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/src', static_url_path='')
 CORS(app)
 
 # MySQL database connection details
@@ -22,13 +23,16 @@ def submit_form():
         conn = mysql.connector.connect(**db_config)
         cursor = conn.cursor()
 
-        # Assuming you have a table named 'form_responses' with columns matching form fields
+        # Update your query to include new fields
         query = (
-            "INSERT INTO form_responses (title, firstName, lastName, institutionName, state, city, phoneNumber, email) "
-            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+            "INSERT INTO form_responses (title, firstName, lastName, pronouns, institute, state, city, zip, phoneNumber, email, engagementA, functionA, notes, followUpRequested, functionB, informationRequested) "
+            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
         cursor.execute(query, (
-        data['title'], data['firstName'], data['lastName'], data['institutionName'], data['state'], data['city'],
-        data['phoneNumber'], data['email'], q1['question1'], q2['question2'], q3['question3']))
+            data['title'], data['firstName'], data['lastName'], data['pronouns'], data['institute'],
+            data['state'], data['city'], data['zip'], data['phoneNumber'], data['email'],
+            data['engagementA'], data['functionA'], data['notes'],
+            data['followUpRequested'], data['functionB'], data['informationRequested']
+        ))
 
         conn.commit()
         return jsonify({'message': 'Form submitted successfully'}), 200
@@ -40,5 +44,13 @@ def submit_form():
             cursor.close()
             conn.close()
 
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def serve(path):
+#     if path != "" and os.path.exists(app.static_folder + '/' + path):
+#         return send_from_directory(app.static_folder, path)
+#     else:
+#         return send_from_directory(app.static_folder, 'index.html')
+
 if __name__ == '__main__':
-    app.run(host='localhost', port='3000', debug=True)
+    app.run(host='localhost', port='5000', debug=True)
